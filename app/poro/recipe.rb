ab90ef8b66b313ac55e55027f5a8411d0e9b9a06
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-04 15:16:14 -0800

    REformat

--- app/poro/recipe.rb
+++ app/poro/recipe.rb
@@ -5,7 +5,6 @@ class Recipe
     @id = nil
     @title = data[:recipe][:label]
     @url = data[:recipe][:url]
-    # @country = data[:recipe][:shareAs].split('/')[-1]
     @country = country
     @image = data[:recipe][:image]
   end
