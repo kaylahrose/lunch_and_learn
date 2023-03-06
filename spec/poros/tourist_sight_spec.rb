40160158ec2759a96c2a632e60c15013952e149b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-06 09:45:04 -0800

    Test tourist sight

---
+++ spec/poros/tourist_sight_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe TouristSight do
+  it 'creates a tourist sight object' do
+    json_response = File.read("./spec/fixtures/france_tourist_sights.json")
+    data = JSON.parse(json_response, symbolize_names: true)[:features].first[:properties]
+    tourist_sight = TouristSight.new(data)
+
+    expect(tourist_sight).to be_a TouristSight
+    expect(tourist_sight.id).to eq(nil)
+    expect(tourist_sight.name).to eq("Tour de l'horloge")
+    expect(tourist_sight.address).to eq("Tour de l'horloge, Allée de l'Horloge, 23200 Aubusson, France")
+    expect(tourist_sight.place_id).to eq("51d28de2694b5901405998a9a0ad8afa4640f00102f901db2e6f0d00000000920311546f7572206465206c27686f726c6f6765")
+  end
+end
