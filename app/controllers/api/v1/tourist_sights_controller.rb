4f5d6c29a51ad7e62311cd05b0cea8a1c00e7f66
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-06 08:46:33 -0800

    Create tourist sight poro

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -8,6 +8,10 @@ class Api::V1::TouristSightsController < ApplicationController
     conn = Faraday.new("https://api.geoapify.com")
     response = conn.get("/v2/places?categories=tourism.sights&apiKey=8cb8b106c84c4e028afa60ec6c356327&filter=circle:2.0,46.0,20000")
     data = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    
+    data[:features].each do |feature|
+      TouristSight.new(feature[:properties])
+      require 'pry'; binding.pry
+    end
   end
 end
---
+++ app/poro/tourist_sight.rb
@@ -0,0 +1,5 @@
+class TouristSight
+  def initialize(data)
+  
+  end
+end
