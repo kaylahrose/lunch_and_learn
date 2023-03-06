5aaeef8fb5468d409de2a5ead20903ccd2df55e9
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-06 09:31:15 -0800

    Enable webmock

--- spec/requests/api/v1/tourist_sights_request_spec.rb
+++ spec/requests/api/v1/tourist_sights_request_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe 'tourist sights API' do
   context 'GET "/api/v1/tourist_sights"' do
     it 'sends a collection of tourist sights' do
-      WebMock.allow_net_connect!
+      # WebMock.allow_net_connect!
       json_response = File.open("./spec/fixtures/france_details.json")
-      # stub_request(:get, "https://restcountries.com/v3.1/name/France").
-      # with(
-      #   headers: {
-      #   'Accept'=>'*/*',
-      #   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      #   'User-Agent'=>'Faraday v2.7.4'
-      #   }).
-      # to_return(status: 200, body: json_response, headers: {})
+      stub_request(:get, "https://restcountries.com/v3.1/name/France").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.7.4'
+        }).
+      to_return(status: 200, body: json_response, headers: {})
 
-      # france_tourist_sights = File.open("./spec/fixtures/france_tourist_sights.json") 
+      france_tourist_sights = File.open("./spec/fixtures/france_tourist_sights.json") 
 
-      # stub_request(:get, "https://api.geoapify.com/v2/places?apiKey=#{ENV['places_api']}&categories=tourism.sights&filter=circle:2.0,46.0,20000&limit=500").
-      # with(
-      #   headers: {
-      #   'Accept'=>'*/*',
-      #   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      #   'User-Agent'=>'Faraday v2.7.4'
-      #   }).
-      # to_return(status: 200, body: france_tourist_sights, headers: {})
+      stub_request(:get, "https://api.geoapify.com/v2/places?apiKey=#{ENV['places_api']}&categories=tourism.sights&filter=circle:2.0,46.0,20000&limit=500").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.7.4'
+        }).
+      to_return(status: 200, body: france_tourist_sights, headers: {})
 
   
 
