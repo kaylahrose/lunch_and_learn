26232a7fabf5c2598b57fcac76dad07d227a4b63
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-06 10:10:58 -0800

    Test tourist sights facade

---
+++ spec/facades/tourist_sights_facade.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe TouristSightsFacade do
+  it '#get_tourist_sights' do
+    json_response = File.open("./spec/fixtures/france_details.json")
+    stub_request(:get, "https://restcountries.com/v3.1/name/France").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.4'
+      }).
+    to_return(status: 200, body: json_response, headers: {})
+
+    france_tourist_sights = File.open("./spec/fixtures/france_tourist_sights.json") 
+    stub_request(:get, "https://api.geoapify.com/v2/places?apiKey=#{ENV['places_api']}&categories=tourism.sights&filter=circle:2.0,46.0,20000&limit=500").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.4'
+      }).
+    to_return(status: 200, body: france_tourist_sights, headers: {})
+
+    tourist_sights = TouristSightsFacade.get_tourist_sights("France")
+
+    expect(tourist_sights).to be_a Array
+    expect(tourist_sights.first).to be_a TouristSight
+  end
+end
