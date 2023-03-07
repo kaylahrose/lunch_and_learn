500c233c3198caac255240631563fd71946fd0b0
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-03-06 16:28:32 -0800

    Reformat

--- app/poro/learning_resource.rb
+++ app/poro/learning_resource.rb
@@ -1,7 +1,7 @@
 class LearningResource
   attr_reader :id, :country, :video, :images
+  
   def initialize(country, video, images)
-    # require 'pry'; binding.pry
     @id = nil
     @country = country
     @video = video
